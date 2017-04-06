e833c5af2366051f546f281b4842aa4087072636
ishikawa <hishikawa@sonix.asia>
2017-04-06 16:56:03 +0900

    add app

---
+++ app/src/main/java/sonix/android/tokyometroapp/activities/OperationActivity.java
@@ -0,0 +1,13 @@
+package sonix.android.tokyometroapp.activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import sonix.android.tokyometroapp.R;
+
+/**
+ * Created by hiroki.ishikawa on 2017/04/06.
+ */
+
+@EActivity(R.layout.activity_operation)
+public class OperationActivity extends BaseActivity {
+}
